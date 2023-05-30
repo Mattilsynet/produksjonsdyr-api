7b7bef5da26ffa252de51b4ea5b1911479c47eb5
wienmari <mari.wien@mattilsynet.no>
2023-05-30 16:17:13 +0200

    Oppsett av openapigenerator og modul for generering av kotlin kode

---
+++ kotlin-spring/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    id("org.springframework.boot") version "3.1.0"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.8.21"
+    kotlin("plugin.spring") version "1.8.21"
+    id ("org.openapi.generator") version "5.1.1"
+    id ("java-library")
+}
+
+group = "no.mattilsynet"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation ("org.openapitools:openapi-generator-gradle-plugin:6.2.1"){
+        exclude(group = "org.slf4j", module = "slf4j-simple")
+        exclude(group = "ch.qos.logback", module = "logback-classic")
+    }
+    implementation("javax.servlet:servlet-api:2.5")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+openApiGenerate {
+    generatorName.set("kotlin-spring")
+    outputDir.set("$buildDir/generated")
+    ignoreFileOverride.set("$rootDir/.openapi-generator-ignore")
+    inputSpec.set("$rootDir/spec.yaml")
+    additionalProperties.set(mapOf("apiFirst" to "true"))
+    apiPackage.set("no.mattilsynet.api")
+    modelPackage.set("no.mattilsynet.model")
+    configOptions.set(mapOf(
+        "interfaceOnly" to "true",
+        "serializableModel" to "true",
+        "useBeanValidation" to "true",
+        "performBeanValuation" to "true",
+        "enumPropertyNaming" to "UPPERCASE"
+    ))
+    sourceSets.getByName(SourceSet.MAIN_SOURCE_SET_NAME).kotlin.srcDir("$buildDir/generated/src/main/kotlin")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+
+tasks.jar {
+    archiveBaseName.set("pdyrapi-kotlin-spring")
+    archiveClassifier.set("")
+    isEnabled = true
+}
+
+tasks.bootJar {
+    isEnabled = false
+}
+
