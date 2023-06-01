3f7147080014353b1895971ef9fe7b319956c49a
Eljar.Haugen@mattilsynet.no <Eljar.Haugen@mattilsynet.no>
2023-06-01 09:13:14 +0200

    Modified 2 files

--- .spectral.mjs
+++ .spectral.mjs
@@ -1,2 +1,2 @@
-#import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4ODU3Mg/spectral.js?branch_slug=master&token=be54c806-c15a-436e-9d7a-9de4481d2195";
-#export default { extends: ruleset };
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4ODU3Mg/spectral.js?branch_slug=master&token=be54c806-c15a-436e-9d7a-9de4481d2195";
+export default { extends: ruleset };
