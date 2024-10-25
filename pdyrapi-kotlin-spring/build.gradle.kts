85f3c7157552cf43b920fd26b59ae919b1c220be
caviarbag <per-odin.eiterjord@mattilsynet.no>
2024-10-25 18:33:57 +0200

    PD-870: oppgraderer openapi-generator-gradle-version

--- pdyrapi-kotlin-spring/build.gradle.kts
+++ pdyrapi-kotlin-spring/build.gradle.kts
@@ -3,11 +3,13 @@ plugins {
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.8.21"
     kotlin("plugin.spring") version "1.8.21"
-    id ("org.openapi.generator") version "6.4.0"
-    id ("java-library")
+    id("org.openapi.generator") version "6.4.0"
+    id("java-library")
     id("maven-publish")
     id("org.sonarqube") version "4.2.1.3168"
 }
+
+val openapiGeneratorVersion by extra("7.9.0")
 
 group = "no.mattilsynet.produksjonsdyr"
 version = "0.0.62-SNAPSHOT"
@@ -22,7 +24,7 @@ dependencies {
     implementation("org.springframework:spring-web:6.0.6")
     implementation("javax.servlet:servlet-api:2.5")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation ("org.openapitools:openapi-generator-gradle-plugin:6.2.1"){
+    implementation("org.openapitools:openapi-generator-gradle-plugin:$openapiGeneratorVersion") {
         exclude(group = "org.slf4j", module = "slf4j-simple")
         exclude(group = "ch.qos.logback", module = "logback-classic")
     }
@@ -30,38 +32,44 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-
-val openapiSpecs = mapOf(
-    "berikelseapi" to "reference/BerikelseAPI.yaml",
-    "frontendsupportapi" to "reference/FrontendSupportAPI.yaml",
-    "brukerinfoapi" to "reference/Brukerinfo.yaml",
-    "ansvarapi" to "reference/Ansvar.yaml",
-    "anleggapi" to "reference/Anlegg.yaml",
-    "oppsummering" to "reference/Oppsummering.yaml",
-    "anleggrapportering" to "reference/AnleggRapportering.yaml",
-    "vedlikehold" to "reference/Vedlikehold.yaml"
-)
+val openapiSpecs =
+    mapOf(
+        "berikelseapi" to "reference/BerikelseAPI.yaml",
+        "frontendsupportapi" to "reference/FrontendSupportAPI.yaml",
+        "brukerinfoapi" to "reference/Brukerinfo.yaml",
+        "ansvarapi" to "reference/Ansvar.yaml",
+        "anleggapi" to "reference/Anlegg.yaml",
+        "oppsummering" to "reference/Oppsummering.yaml",
+        "anleggrapportering" to "reference/AnleggRapportering.yaml",
+        "vedlikehold" to "reference/Vedlikehold.yaml",
+    )
 
 openapiSpecs.forEach {
-	tasks.create("openApiGenerate-${it.key}", org.openapitools.generator.gradle.plugin.tasks.GenerateTask::class) {
-		ignoreFileOverride.set("$rootDir/.openapi-generator-ignore")
-		generatorName.set("kotlin-spring")
-		additionalProperties.set(mapOf("apiFirst" to "true",
-                "removeEnumValuePrefix" to "false"))
-		inputSpec.set("$rootDir/${it.value}")
-		outputDir.set("$buildDir/generated")
-		apiPackage.set("no.mattilsynet.api")
-		modelPackage.set("no.mattilsynet.model")
-		modelNameSuffix.set("Dto")
-		configOptions.set(mapOf(
-			"interfaceOnly" to "true",
-			"serializableModel" to "true",
-			"useBeanValidation" to "true",
-			"performBeanValuation" to "true",
-			"enumPropertyNaming" to "UPPERCASE"
-		))
-		sourceSets.getByName(SourceSet.MAIN_SOURCE_SET_NAME).kotlin.srcDir("$buildDir/generated/src/main/kotlin")
-	}
+    tasks.create("openApiGenerate-${it.key}", org.openapitools.generator.gradle.plugin.tasks.GenerateTask::class) {
+        ignoreFileOverride.set("$rootDir/.openapi-generator-ignore")
+        generatorName.set("kotlin-spring")
+        additionalProperties.set(
+            mapOf(
+                "apiFirst" to "true",
+                "removeEnumValuePrefix" to "false",
+            ),
+        )
+        inputSpec.set("$rootDir/${it.value}")
+        outputDir.set("$buildDir/generated")
+        apiPackage.set("no.mattilsynet.api")
+        modelPackage.set("no.mattilsynet.model")
+        modelNameSuffix.set("Dto")
+        configOptions.set(
+            mapOf(
+                "interfaceOnly" to "true",
+                "serializableModel" to "true",
+                "useBeanValidation" to "true",
+                "performBeanValuation" to "true",
+                "enumPropertyNaming" to "UPPERCASE",
+            ),
+        )
+        sourceSets.getByName(SourceSet.MAIN_SOURCE_SET_NAME).kotlin.srcDir("$buildDir/generated/src/main/kotlin")
+    }
 }
 tasks.register("openApiGenerate-task") { dependsOn(openapiSpecs.keys.map { "openApiGenerate-$it" }) }
 
@@ -71,7 +79,7 @@ tasks.getByName<Test>("test") {
 
 tasks.jar {
     isEnabled = true
-    archiveClassifier.set("") //fjerner plain fra navnet til jar-filen
+    archiveClassifier.set("") // fjerner plain fra navnet til jar-filen
 }
 
 tasks.bootJar {
@@ -99,4 +107,3 @@ publishing {
         }
     }
 }
-
