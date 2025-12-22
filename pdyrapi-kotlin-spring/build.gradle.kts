02ba34bbcbe130cfdb3732f972ffa3210a7ed3b5
Per-Odin Eiterjord <per-odin.eiterjord@mattilsynet.no>
2025-12-22 11:30:56 +0100

    tester..

--- pdyrapi-kotlin-spring/build.gradle.kts
+++ pdyrapi-kotlin-spring/build.gradle.kts
@@ -1,12 +1,14 @@
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
+
 plugins {
     id("org.springframework.boot") version "3.5.9"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("jvm") version "2.3.0"
     kotlin("plugin.spring") version "2.3.0"
-    id("org.openapi.generator") version "6.4.0"
-    id("java-library")
+    id("org.openapi.generator") version "7.17.0"
     id("maven-publish")
     id("org.sonarqube") version "latest.release"
+    `java-library`
 }
 
 group = "no.mattilsynet.produksjonsdyr"
@@ -20,14 +22,20 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework:spring-web:6.0.6")
-    implementation("javax.servlet:servlet-api:2.5")
+    implementation("org.springframework:spring-web:7.0.2")
+    implementation("io.swagger.core.v3:swagger-annotations:2.2.41")
+    implementation("io.swagger.core.v3:swagger-models:2.2.41")
+    // implementation("org.hibernate.validator:hibernate-validator:8.0.1.Final")
+    implementation("jakarta.validation:jakarta.validation-api:3.1.1")
+
+    // implementation("jakarta.servlet:jakarta.servlet-api:6.1.0")
+    // implementation("javax.servlet:servlet-api:4.0.1")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.openapitools:openapi-generator-gradle-plugin:6.2.1") {
-        exclude(group = "org.slf4j", module = "slf4j-simple")
-        exclude(group = "ch.qos.logback", module = "logback-classic")
-    }
-    implementation("javax.servlet:servlet-api:2.5")
+    implementation("org.openapitools:openapi-generator-gradle-plugin:7.14.0")
+    //    exclude(group = "org.slf4j", module = "slf4j-simple")
+    //    exclude(group = "ch.qos.logback", module = "logback-classic")
+    // }
+    // implementation("javax.servlet:servlet-api:2.5")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
@@ -45,17 +53,24 @@ val openapiSpecs =
     )
 
 openapiSpecs.forEach {
-    tasks.create("openApiGenerate-${it.key}", org.openapitools.generator.gradle.plugin.tasks.GenerateTask::class) {
+    tasks.register<GenerateTask>("openApiGenerate-${it.key}") {
+        group = "openapi"
         ignoreFileOverride.set("$rootDir/.openapi-generator-ignore")
         generatorName.set("kotlin-spring")
         additionalProperties.set(
             mapOf(
                 "apiFirst" to "true",
                 "removeEnumValuePrefix" to "false",
+                "useJakartaEe" to "true",
             ),
         )
         inputSpec.set("$rootDir/${it.value}")
-        outputDir.set("$buildDir/generated")
+        outputDir.set(
+            layout.buildDirectory
+                .dir("generated")
+                .get()
+                .asFile.absolutePath,
+        )
         apiPackage.set("no.mattilsynet.api")
         modelPackage.set("no.mattilsynet.model")
         modelNameSuffix.set("Dto")
@@ -68,13 +83,28 @@ openapiSpecs.forEach {
                 "enumPropertyNaming" to "UPPERCASE",
             ),
         )
-        sourceSets.getByName(SourceSet.MAIN_SOURCE_SET_NAME).kotlin.srcDir("$buildDir/generated/src/main/kotlin")
+        sourceSets
+            .getByName(
+                SourceSet.MAIN_SOURCE_SET_NAME,
+            ).kotlin
+            .srcDir(
+                layout.buildDirectory
+                    .dir("generated/src/main/kotlin")
+                    .get()
+                    .asFile.absolutePath,
+            )
     }
 }
 tasks.register("openApiGenerate-task") { dependsOn(openapiSpecs.keys.map { "openApiGenerate-$it" }) }
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+    }
 }
 
 tasks.jar {
